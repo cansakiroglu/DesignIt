0e4e20f6e79552e828ee03d88f93d86846769465
Mehmet Can Şakiroğlu <53798389+cansakiroglu@users.noreply.github.com>
2023-04-02 17:19:14 +0300

    Object Detection - YOLO
    Used yolov7 (https://github.com/WongKinYiu/yolov7) repository as the base,
    Modified detect.py for our case,
    Added query.py for retrieving the resulting class.
    
    Usage:
        Firstly, go into the directory yolov7. (cd yolov7)
        Then, download the model weights. (wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt)   (Not uploaded to the GitHub due to size issues.)
        Lastly, to run detection with a specified image:
        	python query.py with the specified image to run detection on.

---
+++ ObjectDetection/yolov7/detect.py
@@ -0,0 +1,85 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+def detect(source_arg):
+    source, weights, imgsz, trace = source_arg, ['yolov7.pt'], 640, True
+
+    # Initialize
+    set_logging()
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, 640)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=False)[0]
+
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=False)[0]
+        t2 = time_synchronized()
+
+        # Apply NMS1
+        pred = non_max_suppression(pred, 0.25, 0.45, classes=[56,57,58,59,60,61,62,63,68,69,71,72,74,75], agnostic=False) # pred, confthresh, iouthresh ..!
+        t3 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        return pred
